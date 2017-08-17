2aa439ff725084b21c09ffa9f6cd5608834977ca
ZoroZeng <aplexos@163.com>
2017-08-17 14:39:50 +0800

    add heart typewriter.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 64,
+      "value": 128,
       "density": {
         "enable": true,
         "value_area": 800
--- js/zengjf.js
+++ js/zengjf.js
@@ -1,4 +1,14 @@
+var offsetX = $("#loveHeart").width() / 2;
+var offsetY = $("#loveHeart").height() / 2 - 55;
+
 $(function(){  
     var background_music = document.getElementById('background_music');  
     background_music.play(); 
+
+    setTimeout(function () {
+	    startHeartAnimation();
+	}, 5000);
+
+    adjustCodePosition();
+	$("#code").typewriter();
 }); 
