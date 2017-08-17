de67101c6255b5f549a9348bdbfcfa05b3c1ab01
ZoroZeng <aplexos@163.com>
2017-08-17 19:44:41 +0800

    fix iphone can't play background music.

--- js/zengjf.js
+++ js/zengjf.js
@@ -4,7 +4,19 @@ var offsetY = $("#loveHeart").height() / 2 - 55;
 $(function(){  
     var background_music = document.getElementById('background_music');  
     background_music.play(); 
+    background_music.src = "music/try.mp3"; 
+    background_music.play(); 
 
+    $(document).ready(function() {
+        addEventListener('touchstart', function (e) {
+            background_music.src = 'music/try.mp3';
+            background_music.loop = true;
+            background_music.play();
+            background_music.pause();
+            background_music.play();
+        });
+    });
+    
     setTimeout(function () {
 	    startHeartAnimation();
 	}, 5000);
