605d3f2bbbb9d2e03c7000589595355fc2344804
ZoroZeng <aplexos@163.com>
2017-08-18 08:23:57 +0800

    fix iphone can't play background music.

--- js/zengjf.js
+++ js/zengjf.js
@@ -2,6 +2,7 @@ var offsetX = $("#loveHeart").width() / 2;
 var offsetY = $("#loveHeart").height() / 2 - 55;
 
 function force_safari_play_audio() {
+    background_music.src = "http://zorozeng.com/music/try.mp3";
     background_music.loop = true;
     background_music.load();
     background_music.play();
@@ -9,18 +10,18 @@ function force_safari_play_audio() {
 
 function audio_auto_play(id) {
     var audio = document.getElementById(id);  
-    audio.src = "music/try.mp3";
+    audio.play();
+
     audio.addEventListener('play', function() {
         // 当 audio 能够播放后, 移除这个事件
-        window.removeEventListener('touchstart', force_safari_play_audio, false);
-    }, false);
-    audio.play();
+        window.removeEventListener('touchstart', force_safari_play_audio);
+    });
 }
 
 $(function(){  
     // fake-autoplay-audio-ios-safari.html
     //     https://gist.github.com/ufologist/50b4f2768126089c3e11
-    window.addEventListener('touchstart', force_safari_play_audio, false);
+    window.addEventListener('touchstart', force_safari_play_audio);
     audio_auto_play("background_music");
 
     setTimeout(function () {
